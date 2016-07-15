de0d16d10fbe3465ed2b6257728c815f81a60102
arvikachristy <anastasia.arvika@gmail.com>
2016-07-15 17:10:34 +0100

    Setting up the data and model hook

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+let posts = [{
+	title: 'Get a  corgi',
+	author: 'Anna'
+},{
+	title: 'Water the plant',
+	author: "Bob"
+}, {
+	title: 'Laundry day',
+	author: "Charlie"
+}];
+
 export default Ember.Route.extend({
+	model(){
+		    return Ember.RSVP.hash({
+      title: this.get('store').findAll('title')
+    });
+		// return posts;
+	}
 });
