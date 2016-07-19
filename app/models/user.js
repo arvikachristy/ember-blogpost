da8d38b41e8f2a5c3bbf6982336dbea57fbd8e1f
arvikachristy <anastasia.arvika@gmail.com>
2016-07-19 13:29:54 +0100

    Input now works on hard coded data :dancer:

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,9 @@
 import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
+// import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-	title: attr(),
-	author: attr()
-
+	firstname: attr(),
+	lastname: attr(),
+	birthday: attr()
 });
--- app/routes/posts/newpost.js
+++ app/routes/posts/newpost.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+	model: function() {
+		  return this.store.findAll('post');
+	},
+	actions:{
+		submitPost(){
+			var model = this.get('model');
+			var store = this.store
+			var posting = store.createRecord('post', {
+				title: 'Hello',
+				author: 'me'
+		  		// title: model.title,
+		 	 	// author: model.author
+			});
+			posting.save();
+		}
+	}
 });
